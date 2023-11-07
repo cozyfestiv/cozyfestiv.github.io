2f3dc78131622eda3f23e0a9e14432060071c4e0
Gregory Topscher <126685397+cozyfestiv@users.noreply.github.com>
2023-11-07 10:13:12 -0500

    Add Projects and Skills

--- src/App.jsx
+++ src/App.jsx
@@ -1,32 +1,111 @@
 import "./App.css";
 import { NavBar } from "./NavBar";
 import profileVert from "./images/profile-vert.jpg";
+import wlm from "./images/wlm.png";
+import ptables from "./images/periodic-tables.png";
 
 function App() {
   return (
-    <div>
+    <div className="frame">
       <NavBar />
-      <img src={profileVert} className="profile"></img>
-      <h1>
-        Nice to meet you!
-        <br />
-        I'm Greg Topscher.
-      </h1>
+      <div className="introBlock">
+        <div className="bio col">
+          <h1 className="greet">
+            Nice to meet you!
+            <br />
+            I'm Greg Topscher.
+          </h1>
+          <p className="bioCopy">
+            Based in the Philadelphia, I'm a front-end developer passionate
+            about building accessible web apps that users love.
+          </p>
+          <br />
+          <p>CONTACT ME</p>
+        </div>
+        <div className="col">
+          <img src={profileVert} className="profilePic"></img>
+        </div>
+      </div>
+
+      <hr />
+
+      <div className="expBlock">
+        <div className="exp row">
+          <div className="col3">
+            <h2>HTML </h2>
+            <p>1 Year Experience</p>
+          </div>
+          <div className="col3">
+            <h2>CSS </h2>
+            <p>1 Year Experience</p>
+          </div>
+          <div className="col3">
+            <h2>JavaScript </h2>
+            <p>1 Year Experience</p>
+          </div>
+        </div>
+        <div className="exp row">
+          <div className="col3">
+            <h2>Accessibility</h2>
+            <p>1 Year Experience</p>
+          </div>
+          <div className="col3">
+            <h2>React </h2>
+            <p>1 Year Experience</p>
+          </div>
+          <div className="col3">
+            <h2>Express </h2>
+            <p>1 Year Experience</p>
+          </div>
+        </div>
+      </div>
+
+      <div className="projectHead row">
+        <h1>Projects</h1> <p>CONTACT ME</p>
+      </div>
+
+      <div className="project row">
+        <div className="pic">
+          <a
+            href="https://periodic-tables-restaurant-reservation-fo68.onrender.com/"
+            target="_blank"
+          >
+            <img src={ptables} className="projPic"></img>
+          </a>
+          <h3>EXPRESS.JS | NODE.JS | KNEX | GIT </h3>
+          <div class="middle">
+            <div class="text">Restaurant Reservation App</div>
+          </div>
+        </div>
+        <div className="pic">
+          <a
+            href="https://starter-movie-front-19jdwo4to-cozyfestiv.vercel.app/"
+            target="_blank"
+          >
+            <img src={wlm} className="projPic"></img>
+          </a>
+          <h3>EXPRESS.JS | NODE.JS | KNEX | GIT</h3>
+          <div class="middle">
+            <div class="text">Movie Database App</div>
+          </div>
+        </div>
+      </div>
+
+      <div className="project row">
+        <div className="col3">
+          <h2>Accessibility</h2>
+          <p>1 Year Experience</p>
+        </div>
+        <div className="col3">
+          <h2>React </h2>
+          <p>1 Year Experience</p>
+        </div>
+      </div>
 
       <p>
-        Based in the Philadelphia, I'm a front-end developer passionate about
-        building accessible web apps that users love. Contact me HTML 4 Years
-        Experience CSS 4 Years Experience JavaScript 4 Years Experience
-        Accessibility 4 Years Experience React 3 Years Experience Sass 3 Years
-        Experience Projects Contact me Design portfolio HTML CSS View project
-        View code E-learning landing page HTML CSS View project View code Todo
-        web app HTML CSS JavaScript View project View code Entertainment web app
-        HTML CSS JavaScript View project View code Memory Game HTML CSS
-        JavaScript View project View code Art gallery showcase HTML CSS
-        JavaScript View project View code Contact I would love to hear about
-        your project and how I could help. Please fill in the form, and I’ll get
-        back to you as soon as possible. Name Email Message Send message Greg
-        Topscher
+        Contact I would love to hear about your project and how I could help.
+        Please fill in the form, and I’ll get back to you as soon as possible.
+        Name Email Message Send message Greg Topscher
       </p>
     </div>
   );
